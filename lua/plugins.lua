544a90408b24afd1b89f932c1ca2722ffc3c252e
Hugo Bouderlique <bouderliqueh@gmail.com>
2023-07-04 00:26:24 +1000

    move to lazy.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,27 +1,35 @@
-return require("packer").startup(function(use)
-    -- Nvim Packer
-    use("wbthomason/packer.nvim")
+return {
 
-    use { "nvim-telescope/telescope.nvim", tag = "0.1.0", requires = { { "nvim-lua/plenary.nvim" } } }
+    {
+        "nvim-telescope/telescope.nvim",
+        version = "0.1.0",
+        dependencies = { { "nvim-lua/plenary.nvim" } }
+    },
 
     -- Nvim Tree Sitter
-    use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+    {
+        "nvim-treesitter/nvim-treesitter",
+        config = ":TSUpdate"
+    },
 
     -- Discord presence to flex on other people
-    use("andweeb/presence.nvim")
+    "andweeb/presence.nvim",
 
     -- colorscheme
-    use { "rebelot/kanagawa.nvim" }
+    {
+        "rebelot/kanagawa.nvim",
+        lazy = false
+    },
 
-    use {
+    {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v2.x',
-        requires = {
+        dependencies = {
             -- LSP Support
             { 'neovim/nvim-lspconfig' }, -- Required
             {                            -- Optional
                 'williamboman/mason.nvim',
-                run = function()
+                build = function()
                     pcall(vim.cmd, 'MasonUpdate')
                 end,
             },
@@ -32,17 +40,13 @@ return require("packer").startup(function(use)
             { 'hrsh7th/cmp-nvim-lsp' }, -- Required
             { 'L3MON4D3/LuaSnip' },     -- Required
         }
-    }
+    },
 
-    use({
+    {
         "numToStr/Comment.nvim",
         config = function()
-            print("hello??")
             require("Comment").setup()
         end
-    })
+    },
 
-    if packer_bootstrap then
-        require("packer").sync()
-    end
-end)
+}
