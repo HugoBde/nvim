f4d851c5227f8a684b4c94316aadabfff4f27184
bde <bouderliqueh@gmail.com>
2023-09-22 00:07:39 +0100

    move lualine config to its own file

---
+++ after/plugin/lualine.lua
@@ -0,0 +1,33 @@
+require("lualine").setup({
+    options = {
+        section_separators = '',
+        component_separators = '',
+        theme = "codedark", -- todo: make it match colorscheme
+    },
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff' },
+        lualine_c = {
+            {
+                'filename',
+                newfile_status = true,
+                path = 1
+            },
+        },
+
+        lualine_x = { {
+            'diagnostics',
+            icons_enabled = false,
+            sections = { "error", "warn" },
+            update_in_insert = true,
+            always_visible = true,
+        }, 'filetype' },
+        lualine_y = {
+            {
+                'datetime',
+                style = "%H:%M:%S"
+            }
+        },
+        lualine_z = { 'location' }
+    },
+})
