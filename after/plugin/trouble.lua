fafea1eb644701f42134b7b5add2107920c91250
Hugo Bouderlique <bouderliqueh@gmail.com>
2023-09-28 00:47:47 +1000

    rework trouble.nvim keybindings

--- after/plugin/trouble.lua
+++ after/plugin/trouble.lua
@@ -1,6 +1,12 @@
 local trouble = require("trouble")
 
 -- Open Diagnostic window / Trouble.nvim
-vim.keymap.set("n", "<leader>xx", trouble.toggle, { desc = "toggle diagnostic window" })
-vim.keymap.set("n", "<leader>xz", trouble.open, { desc = "open diagnostic window" })
-vim.keymap.set("n", "<leader>xc", trouble.close, { desc = "close diagnostic window" })
+
+require("which-key").register({
+    o = {
+        name = "Trouble",
+        o = { trouble.toggle, "toggle diagnostic window" },
+        i = { trouble.open, "open diagnostic window" },
+        p = { trouble.close, "close diagnostic window" },
+    },
+}, { prefix = "<leader>" })
